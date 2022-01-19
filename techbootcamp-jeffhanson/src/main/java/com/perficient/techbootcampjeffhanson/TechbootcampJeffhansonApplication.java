1f87db46d8b3d8b8f86c2249e0eb94f7e402e0a4
Jeff Hanson <jeff.hanson@perficient.com>
2022-01-19 12:33:10 -0600

    inital commit of initialized spring

---
+++ techbootcamp-jeffhanson/src/main/java/com/perficient/techbootcampjeffhanson/TechbootcampJeffhansonApplication.java
@@ -0,0 +1,13 @@
+package com.perficient.techbootcampjeffhanson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechbootcampJeffhansonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechbootcampJeffhansonApplication.class, args);
+	}
+
+}
---
+++ techbootcamp-jeffhanson/src/test/java/com/perficient/techbootcampjeffhanson/TechbootcampJeffhansonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.perficient.techbootcampjeffhanson;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechbootcampJeffhansonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
